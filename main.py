d760200bd74e9bc4e9f8ecc43fbf0991962f50f5
ruslan <random.ruslan@gmail.com>
2019-05-08 04:01:40 +0300

    Cycles for sign place after dot and for all pictures

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import time
 from pictures import *
 from image_diff import *
 
-WORK_DIR = 'pictures/projectVarya'
-WORK_FILE = 'v02.png'
 
 def get_bin_view(x):
     bn = bin(x)[2:]
     return '0' * (8 - len(bn)) + bn
 
 
-if __name__ == '__main__':
-    for block_side in [8, 16, 32]:
+def start(WORK_FILE, WORK_DIR, sign_place):
+    print('Start ' + WORK_FILE)
+    for block_side in [32, 16, 8]:
         print('Start ' + str(block_side) + 'x' + str(block_side))
         t1 = time.time()
         blocks_dir = os.path.join(WORK_DIR, WORK_FILE.split('.')[0] + '_blocks_' + str(block_side))
@@ -22,8 +21,22 @@ if __name__ == '__main__':
             pass
 
         img_desc = split_picture(WORK_DIR, WORK_FILE, blocks_dir, block_side)
-        create_blocks(blocks_dir)
-        total_comparison(blocks_dir)
+        create_blocks(blocks_dir, sign_place)
+        total_comparison(blocks_dir, sign_place)
         compile_pictures(WORK_DIR, blocks_dir, block_side, img_desc)
         t2 = time.time()
         print('time: ', t2-t1)
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    for sign_place in range(8):
+        for i in range(10):
+            try:
+                file = str(i) + '.png'
+                dirr = '../long/' + str(sign_place) + '/' + str(i)
+                start(file, dirr, sign_place)
+            except Exception as e:
+                print(str(i) + '.png is broken:', e)
+
+    print('TOTAL TIME:', time.time()-t0)
